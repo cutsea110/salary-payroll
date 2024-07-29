078154ef05c3a3eb3d8287891705cc5687d103e1
cutsea110 <cutsea110@gmail.com>
2024-07-29 18:06:00 +0900

    packaging schedule

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use chrono::{Datelike, Days, NaiveDate, Weekday};
+use chrono::{Datelike, NaiveDate, Weekday};
 use core::fmt::Debug;
-use std::{any::Any, cell::RefCell, collections::HashMap, ops::RangeInclusive, rc::Rc};
+use std::{any::Any, cell::RefCell, collections::HashMap, rc::Rc};
 use thiserror::Error;
 use tx_rs::Tx;
 
@@ -331,6 +331,51 @@ mod classification {
     }
 }
 use classification::*;
+
+mod schedule {
+    use chrono::{Datelike, Days, NaiveDate, Weekday};
+    use std::ops::RangeInclusive;
+
+    use crate::domain::PaymentSchedule;
+
+    #[derive(Debug, Clone, Eq, PartialEq)]
+    pub struct MonthlySchedule;
+    impl PaymentSchedule for MonthlySchedule {
+        fn is_pay_date(&self, date: NaiveDate) -> bool {
+            self.is_last_day_of_month(date)
+        }
+        fn get_pay_period(&self, pay_date: NaiveDate) -> RangeInclusive<NaiveDate> {
+            // pay_date should be last_day of month
+            pay_date.with_day(1).unwrap()..=pay_date
+        }
+    }
+    impl MonthlySchedule {
+        pub fn is_last_day_of_month(&self, date: NaiveDate) -> bool {
+            date.month() != date.checked_add_days(Days::new(1)).unwrap().month()
+        }
+    }
+    #[derive(Debug, Clone, Eq, PartialEq)]
+    pub struct WeeklySchedule;
+    impl PaymentSchedule for WeeklySchedule {
+        fn is_pay_date(&self, date: NaiveDate) -> bool {
+            date.weekday() == Weekday::Fri
+        }
+        fn get_pay_period(&self, pay_date: NaiveDate) -> RangeInclusive<NaiveDate> {
+            pay_date.checked_sub_days(Days::new(6)).unwrap()..=pay_date
+        }
+    }
+    #[derive(Debug, Clone, Eq, PartialEq)]
+    pub struct BiweeklySchedule;
+    impl PaymentSchedule for BiweeklySchedule {
+        fn is_pay_date(&self, date: NaiveDate) -> bool {
+            date.weekday() == Weekday::Fri && date.iso_week().week() % 2 == 0
+        }
+        fn get_pay_period(&self, pay_date: NaiveDate) -> RangeInclusive<NaiveDate> {
+            pay_date.checked_sub_days(Days::new(13)).unwrap()..=pay_date
+        }
+    }
+}
+use schedule::*;
 
 #[derive(Debug, Clone, Eq, PartialEq, Error)]
 enum EmployeeUsecaseError {
@@ -846,43 +891,6 @@ trait PaydayTransaction<Ctx>: HaveEmployeeDao<Ctx> {
             }
             Ok(())
         })
-    }
-}
-
-#[derive(Debug, Clone, Eq, PartialEq)]
-struct MonthlySchedule;
-impl PaymentSchedule for MonthlySchedule {
-    fn is_pay_date(&self, date: NaiveDate) -> bool {
-        self.is_last_day_of_month(date)
-    }
-    fn get_pay_period(&self, pay_date: NaiveDate) -> RangeInclusive<NaiveDate> {
-        // pay_date should be last_day of month
-        pay_date.with_day(1).unwrap()..=pay_date
-    }
-}
-impl MonthlySchedule {
-    fn is_last_day_of_month(&self, date: NaiveDate) -> bool {
-        date.month() != date.checked_add_days(Days::new(1)).unwrap().month()
-    }
-}
-#[derive(Debug, Clone, Eq, PartialEq)]
-struct WeeklySchedule;
-impl PaymentSchedule for WeeklySchedule {
-    fn is_pay_date(&self, date: NaiveDate) -> bool {
-        date.weekday() == Weekday::Fri
-    }
-    fn get_pay_period(&self, pay_date: NaiveDate) -> RangeInclusive<NaiveDate> {
-        pay_date.checked_sub_days(Days::new(6)).unwrap()..=pay_date
-    }
-}
-#[derive(Debug, Clone, Eq, PartialEq)]
-struct BiweeklySchedule;
-impl PaymentSchedule for BiweeklySchedule {
-    fn is_pay_date(&self, date: NaiveDate) -> bool {
-        date.weekday() == Weekday::Fri && date.iso_week().week() % 2 == 0
-    }
-    fn get_pay_period(&self, pay_date: NaiveDate) -> RangeInclusive<NaiveDate> {
-        pay_date.checked_sub_days(Days::new(13)).unwrap()..=pay_date
     }
 }
 
