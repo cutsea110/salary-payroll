886178ea8ada980548774caaa313448f896c90a7
cutsea110 <cutsea110@gmail.com>
2024-08-04 11:11:10 +0900

    Revert "handle record_paycheck error"
    This reverts commit 57ea177f8d9de7e2ec714ac380c4f0717470c6ac.

--- src/main.rs
+++ src/main.rs
@@ -545,8 +545,6 @@ mod tx_base {
         AddUnionMemberFailed(EmployeeDaoError),
         #[error("remove union member failed: {0}")]
         RemoveUnionMemberFailed(EmployeeDaoError),
-        #[error("record paycheck failed: {0}")]
-        RecordPayCheckFailed(EmployeeDaoError),
     }
 
     pub trait Transaction<Ctx> {
@@ -1275,10 +1273,7 @@ mod general_tx {
                         let period = emp.get_pay_period(pay_date);
                         let mut pc = PayCheck::new(period);
                         emp.payday(&mut pc);
-                        self.dao()
-                            .record_paycheck(emp.get_emp_id(), pc)
-                            .run(ctx)
-                            .map_err(EmployeeUsecaseError::RecordPayCheckFailed)?;
+                        let _ = self.dao().record_paycheck(emp.get_emp_id(), pc).run(ctx);
                     }
                 }
                 Ok(())
