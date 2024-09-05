fe0f9d7c2913f8d108ea64dad368e93eb11a33c0
cutsea110 <cutsea110@gmail.com>
2024-09-06 05:54:12 +0900

    From trait method instead of function

--- src/main.rs
+++ src/main.rs
@@ -1931,134 +1931,138 @@ trait Transaction<Ctx> {
     fn execute(&mut self) -> Result<(), EmployeeUsecaseError>;
 }
 
-fn from_command(command: Command, db: MockDb) -> Box<dyn Transaction<()>> {
-    match command {
-        Command::AddSalaryEmp {
-            emp_id,
-            name,
-            address,
-            salary,
-        } => Box::new(AddSalariedEmployeeTransactionImpl {
-            db,
-            emp_id,
-            name,
-            address,
-            salary,
-        }),
-        Command::AddHourlyEmp {
-            emp_id,
-            name,
-            address,
-            hourly_rate,
-        } => Box::new(AddHourlyEmployeeTransactionImpl {
-            db,
-            emp_id,
-            name,
-            address,
-            hourly_rate,
-        }),
-        Command::AddCommissionedEmp {
-            emp_id,
-            name,
-            address,
-            salary,
-            commission_rate,
-        } => Box::new(AddCommissionedEmployeeTransactionImpl {
-            db,
-            emp_id,
-            name,
-            address,
-            salary,
-            commission_rate,
-        }),
-        Command::DelEmp { emp_id } => Box::new(DeleteEmployeeTransactionImpl { db, emp_id }),
-        Command::TimeCard {
-            emp_id,
-            date,
-            hours,
-        } => Box::new(TimeCardTransactionImpl {
-            db,
-            emp_id,
-            date,
-            hours,
-        }),
-        Command::SalesReceipt {
-            emp_id,
-            date,
-            amount,
-        } => Box::new(SalesReceiptTransactionImpl {
-            db,
-            emp_id,
-            date,
-            amount,
-        }),
-        Command::ServiceCharge {
-            member_id,
-            date,
-            amount,
-        } => Box::new(ServiceChargeTransactionImpl {
-            db,
-            member_id,
-            date,
-            amount,
-        }),
-        Command::ChgName { emp_id, name } => {
-            Box::new(ChangeNameTransactionImpl { db, emp_id, name })
+impl From<(Command, MockDb)> for Box<dyn Transaction<()>> {
+    fn from((command, db): (Command, MockDb)) -> Box<dyn Transaction<()>> {
+        match command {
+            Command::AddSalaryEmp {
+                emp_id,
+                name,
+                address,
+                salary,
+            } => Box::new(AddSalariedEmployeeTransactionImpl {
+                db,
+                emp_id,
+                name,
+                address,
+                salary,
+            }),
+            Command::AddHourlyEmp {
+                emp_id,
+                name,
+                address,
+                hourly_rate,
+            } => Box::new(AddHourlyEmployeeTransactionImpl {
+                db,
+                emp_id,
+                name,
+                address,
+                hourly_rate,
+            }),
+            Command::AddCommissionedEmp {
+                emp_id,
+                name,
+                address,
+                salary,
+                commission_rate,
+            } => Box::new(AddCommissionedEmployeeTransactionImpl {
+                db,
+                emp_id,
+                name,
+                address,
+                salary,
+                commission_rate,
+            }),
+            Command::DelEmp { emp_id } => Box::new(DeleteEmployeeTransactionImpl { db, emp_id }),
+            Command::TimeCard {
+                emp_id,
+                date,
+                hours,
+            } => Box::new(TimeCardTransactionImpl {
+                db,
+                emp_id,
+                date,
+                hours,
+            }),
+            Command::SalesReceipt {
+                emp_id,
+                date,
+                amount,
+            } => Box::new(SalesReceiptTransactionImpl {
+                db,
+                emp_id,
+                date,
+                amount,
+            }),
+            Command::ServiceCharge {
+                member_id,
+                date,
+                amount,
+            } => Box::new(ServiceChargeTransactionImpl {
+                db,
+                member_id,
+                date,
+                amount,
+            }),
+            Command::ChgName { emp_id, name } => {
+                Box::new(ChangeNameTransactionImpl { db, emp_id, name })
+            }
+            Command::ChgAddress { emp_id, address } => Box::new(ChangeAddressTransactionImpl {
+                db,
+                emp_id,
+                address,
+            }),
+            Command::ChgSalaried { emp_id, salary } => {
+                Box::new(ChangeSalaryTransactionImpl { db, emp_id, salary })
+            }
+            Command::ChgHourly {
+                emp_id,
+                hourly_rate,
+            } => Box::new(ChangeHourlyTransactionImpl {
+                db,
+                emp_id,
+                hourly_rate,
+            }),
+            Command::ChgCommissioned {
+                emp_id,
+                salary,
+                commission_rate,
+            } => Box::new(ChangeCommissionedTransactionImpl {
+                db,
+                emp_id,
+                salary,
+                commission_rate,
+            }),
+            Command::ChgHold { emp_id } => Box::new(ChangeHoldTransactionImpl { db, emp_id }),
+            Command::ChgDirect {
+                emp_id,
+                bank,
+                account,
+            } => Box::new(ChangeDirectTransactionImpl {
+                db,
+                emp_id,
+                bank,
+                account,
+            }),
+            Command::ChgMail { emp_id, address } => Box::new(ChangeMailTransactionImpl {
+                db,
+                emp_id,
+                address,
+            }),
+            Command::ChgMember {
+                emp_id,
+                member_id,
+                dues,
+            } => Box::new(ChangeUnionMemberTransactionImpl {
+                db,
+                emp_id,
+                member_id,
+                dues,
+            }),
+            Command::ChgNoMember { emp_id } => {
+                Box::new(ChangeNoMemberTransactionImpl { db, emp_id })
+            }
+            Command::Payday { pay_date } => Box::new(PaydayTransactionImpl { db, pay_date }),
         }
-        Command::ChgAddress { emp_id, address } => Box::new(ChangeAddressTransactionImpl {
-            db,
-            emp_id,
-            address,
-        }),
-        Command::ChgSalaried { emp_id, salary } => {
-            Box::new(ChangeSalaryTransactionImpl { db, emp_id, salary })
-        }
-        Command::ChgHourly {
-            emp_id,
-            hourly_rate,
-        } => Box::new(ChangeHourlyTransactionImpl {
-            db,
-            emp_id,
-            hourly_rate,
-        }),
-        Command::ChgCommissioned {
-            emp_id,
-            salary,
-            commission_rate,
-        } => Box::new(ChangeCommissionedTransactionImpl {
-            db,
-            emp_id,
-            salary,
-            commission_rate,
-        }),
-        Command::ChgHold { emp_id } => Box::new(ChangeHoldTransactionImpl { db, emp_id }),
-        Command::ChgDirect {
-            emp_id,
-            bank,
-            account,
-        } => Box::new(ChangeDirectTransactionImpl {
-            db,
-            emp_id,
-            bank,
-            account,
-        }),
-        Command::ChgMail { emp_id, address } => Box::new(ChangeMailTransactionImpl {
-            db,
-            emp_id,
-            address,
-        }),
-        Command::ChgMember {
-            emp_id,
-            member_id,
-            dues,
-        } => Box::new(ChangeUnionMemberTransactionImpl {
-            db,
-            emp_id,
-            member_id,
-            dues,
-        }),
-        Command::ChgNoMember { emp_id } => Box::new(ChangeNoMemberTransactionImpl { db, emp_id }),
-        Command::Payday { pay_date } => Box::new(PaydayTransactionImpl { db, pay_date }),
     }
 }
 
@@ -3135,7 +3139,7 @@ impl TransactionSource<()> for TestPayrollApp {
             .parse(&self.input)
             .map(|(ts, _)| {
                 ts.into_iter()
-                    .map(|t| from_command(t, self.db.clone()))
+                    .map(|t| (t, self.db.clone()).into())
                     .collect::<Vec<_>>()
             })
             .unwrap_or_default()
