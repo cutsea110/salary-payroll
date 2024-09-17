29011afe466d5241f201d4a12773424de02b2161
cutsea110 <cutsea110@gmail.com>
2024-09-18 07:30:16 +0900

    refactor

--- mock-app/src/lib.rs
+++ mock-app/src/lib.rs
@@ -9,18 +9,18 @@ pub struct TestPayrollApp {
     db: MockDb,
     file_path: PathBuf,
 }
-impl TransactionApplication<()> for TestPayrollApp {
-    fn tx_source(&self) -> impl TransactionSource<()> {
-        let input = std::fs::read_to_string(&self.file_path).expect("read script file");
-
-        TextParserTransactionSource::new(self.db.clone(), input)
-    }
-}
 impl TestPayrollApp {
     pub fn new(file_name: &str) -> Self {
         Self {
             db: MockDb::new(),
             file_path: file_name.into(),
         }
+    }
+}
+impl TransactionApplication<()> for TestPayrollApp {
+    fn tx_source(&self) -> impl TransactionSource<()> {
+        let input = std::fs::read_to_string(&self.file_path).expect("read script file");
+
+        TextParserTransactionSource::new(self.db.clone(), input)
     }
 }
